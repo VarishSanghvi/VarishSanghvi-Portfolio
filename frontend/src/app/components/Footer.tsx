415650adc64b056eea4f0c731f32f2b782799019
VarishSanghv <varishsanghvi155@gmail.com>
2025-03-18 00:15:41 +0530

    Updated Footer in frontend

--- frontend/src/app/components/Footer.tsx
+++ frontend/src/app/components/Footer.tsx
@@ -2,7 +2,7 @@
 export default function Footer() {
     return (
       <footer className="bg-gray-100 text-center py-4 mt-10 border-t">
-        <p className="text-sm text-gray-600">&copy; {new Date().getFullYear()} Code & Creativity. All rights reserved.</p>
+        <p className="text-sm text-gray-600">&copy; {new Date().getFullYear()} Code & Creativity | Varish Sanghvi. All rights reserved.</p>
       </footer>
     );
   }
